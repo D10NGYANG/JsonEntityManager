5161edfe17bbf89e8db64f2e24cac2c795949084
yangdilong <d10ngyang@163.com>
2020-02-22 16:37:36 +0800

    添加注释；

--- app/src/main/java/com/dlong/jsonentitymanager/TestInfo.kt
+++ app/src/main/java/com/dlong/jsonentitymanager/TestInfo.kt
@@ -8,14 +8,23 @@ import com.dlong.jsonentitylib.annotation.DLField
  * @date on 2019-10-23 16:53
  */
 data class TestInfo (
+
+    // 使用nameInJson标注在JSON中对应的字段
     @DLField(nameInJson = "int1")
     var intT: Int = 0,
+
     @DLField
     var stringT: String = "",
+
+    // 这是一个普通Array，用于读取JSON中的JSONArray
     @DLField
     var listT: List<String> = emptyList(),
+
+    // 这是另一个继承了BaseJsonEntity()的变量
     @DLField
     var test2: Test2Info = Test2Info(),
+
+    // 这是继承了BaseJsonEntity()的变量列表
     @DLField
     var testList: List<Test2Info> = emptyList()
 ) : BaseJsonEntity()
